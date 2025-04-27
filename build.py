995c4735aedba95fa9593fb53977f3887eac287d
IndiaPaleAle <154647446+Maltsburg@users.noreply.github.com>
2025-04-27 11:55:53 -0700

    release workflow

--- build.py
+++ build.py
@@ -1,7 +1,6 @@
 import json
 import uuid
 import os
-import zipfile
 import requests
 
 with open('manifest.json', 'r') as file:
@@ -63,26 +62,3 @@ def get_icon():
         f.write(requests.get(f"https://api.mcstatus.io/v2/icon/akoot.co").content)
 
 get_icon()
-
-# zip everything
-def zip_directory(src_dir, zip_name, exclude_folders=[], exclude_files=[]):
-    with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        for root, dirs, files in os.walk(src_dir):
-            # exclude the specified folders
-            dirs[:] = [d for d in dirs if d not in exclude_folders]
-
-            for file in files:
-                # skip the zip file being created
-                if file == zip_name:
-                    continue
-
-                if file in exclude_files:
-                    continue
-
-                file_path = os.path.join(root, file)
-                zipf.write(file_path, os.path.relpath(file_path, src_dir))
-
-folders_to_exclude = ['json', '.git', ".idea"]
-files_to_exclude = ['build.py', 'build.bat']
-
-zip_directory('./', 'bedrock_pack.zip', folders_to_exclude, files_to_exclude)
